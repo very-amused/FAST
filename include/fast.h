882da1d898d56680f33afa75727be19a0dd8e62c
very-amused <very-amused@pm.me>
2025-08-13 15:58:39 -0400

    Prototype some FastStream methods

---
+++ include/fast.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "stream.h"
---
+++ include/stream.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <stdint.h>
+#include <stdbool.h>
+
+// A fast STREAM instance which consumes audio frames on a clock,
+// simulating an audio stream connected to a real audio server such as pulseaudio or pipewire
+//
+// This is the terminal point of audio for fast: read into a black hole.
+typedef struct FastStream FastStream;
+
+// Settings for creating a FastStream
+typedef struct FastStreamSettings {
+	uint32_t buffer_ms; // ms of audio to buffer
+	uint8_t sample_byte_size; // byte size of one audio sample (e.g 2 for 16-bit samples)
+} FastStreamSettings;
+
+// Allocate and initialize a new FastStream
+// NOTE: The stream starts in a paused/corked state and is not started until [FastStream_start] is called.
+//
+// Returns NULL on error.
+FastStream *FastStream_new();
+// Stop, deinitialize and free a FastStream
+void FastStream_free(FastStream *stream);
+
+// Start a FastStream, causing it to start reading audio from its buffer and requesting audio frames
+//
+// Returns 0 on success, nonzero on error
+int FastStream_start(FastStream *stream);
+
+// Play/pause a FastStream, blocking until the desired play/pause state is achieved
+int FastStream_play(FastStream *stream, bool play);
