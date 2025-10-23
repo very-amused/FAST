d582f73492191f26f205ade49da93d31c85ed6ed
very-amused <very-amused@pm.me>
2025-10-23 00:19:09 -0400

    Use cwd for outfile path

--- build_release.py
+++ build_release.py
@@ -5,11 +5,9 @@ from pathlib import Path
 from sys import argv
 
 src_root = Path(os.environ['MESON_PROJECT_SOURCE_ROOT'])
-build_root = Path(os.environ['MESON_PROJECT_BUILD_ROOT'])
 outfile = Path(argv[1])
 
 subprocess.run(['cargo', 'build', '--release'], cwd=src_root).check_returncode()
-os.makedirs((build_root / outfile).parent, exist_ok=True)
 subprocess.run(['cp',
                 src_root / 'target' / 'release' / outfile.name,
-                build_root / outfile]).check_returncode()
+                outfile]).check_returncode()
