a6a7f4082959dd4f606056fca8210801385b123d
very-amused <very-amused@pm.me>
2025-10-24 02:05:49 -0400

    Restructure FastLoop

--- include/loop.h
+++ include/loop.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <stdint.h>
 
+#include "server.h"
+
 // An asynchronous event loop analogue that provides a lock needed to interact with a FAST server.
 // Callbacks are automatically run with the lock acquired. Thus, acquiring a FastLoop lock outside a callback
 // will prevent callbacks from running while the lock is held.
@@ -11,7 +13,7 @@ typedef struct FastLoop FastLoop;
 // Once allocated, the loop is ready for use.
 //
 // Returns NULL on error.
-FastLoop *FastLoop_new();
+FastLoop *FastLoop_new(FastServer *srv);
 // Deinitialize and free a FastLoop.
 void FastLoop_free(FastLoop *loop);
 
--- src/floop.rs
+++ src/floop.rs
@@ -1,63 +1,57 @@
 #![allow(dead_code)]
 
 use tokio::task::JoinHandle;
-use tokio::runtime::{Runtime as TokioRuntime, Builder as RuntimeBuilder};
-use parking_lot::Mutex;
-use std::{io,mem};
+use tokio::runtime::{Runtime, Builder as RuntimeBuilder};
+use parking_lot::{Mutex,MutexGuard};
+use std::{mem};
 use std::sync::Arc;
+
+use crate::server::FastServer;
 
 /// An async-loop emulator providing a mutex over FAST operations.
 pub struct FastLoop {
-	runtime: Arc<TokioRuntime>,
-	lock: Mutex<()>,
+	data: Mutex<FastLoopData>
+}
 
-	// Callback handling
-	// Thread for an active callback routine. Only 1 at a time!
-	// When a callback is spawned, call [callback_wait] to await its JoinHandle before unlocking the
-	// loop.
+struct FastLoopData {
+	runtime: Arc<Runtime>,
 	callback_task: Option<JoinHandle<()>>
 }
 
 /// Create a new FastLoop ready for use.
 #[unsafe(no_mangle)]
-pub extern "C" fn FastLoop_new() -> *mut FastLoop {
-	// Initialize Tokio async runtime
-	let runtime = match new_runtime() {
-		Ok(rt) => rt,
-		Err(err) => {
-			eprintln!("Failed to initialize Tokio runtime: {}", err);
-			return std::ptr::null_mut();
-		}
+pub extern "C" fn FastLoop_new(srv_ptr: *mut FastServer) -> *mut FastLoop {
+	let srv = unsafe { &*srv_ptr };
+
+	let inner = FastLoopData {
+		runtime:  srv.0.clone(),
+		callback_task: None
 	};
 
 	let floop = Box::new(FastLoop {
-		runtime: Arc::new(runtime),
-		lock: Mutex::new(()),
-		callback_task: None
+		data: Mutex::new(inner)
 	});
 
 	Box::leak(floop)
 }
 
-/// Initialize a Tokio runtime capable of powering a FastStream
-fn new_runtime() -> io::Result<TokioRuntime>{
-	RuntimeBuilder::new_multi_thread()
-		.worker_threads(4) // stream_task + callback_task + growing room
-		.enable_all()
-		.build()
-}
-
 #[unsafe(no_mangle)]
 pub extern "C" fn FastLoop_free(floop_ptr: *mut FastLoop) {
-	let _floop = unsafe { Box::from_raw(floop_ptr) };
+	let floop = unsafe { Box::from_raw(floop_ptr) };
+
+	// Abort any callback that's running
+	let mut inner = floop.data.lock();
+	if let Some(thr) = &inner.callback_task {
+		thr.abort();
+		inner.callback_task = None;
+	}
 
 	// floop gets dropped
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn FastLoop_lock(floop_ptr: *mut FastLoop) {
-	let floop = unsafe { &*floop_ptr };
-	let guard = floop.lock.lock();
+	let guard = unsafe { (&*floop_ptr).data.lock() };
 
 	mem::forget(guard);
 }
@@ -65,5 +59,5 @@ pub extern "C" fn FastLoop_lock(floop_ptr: *mut FastLoop) {
 #[unsafe(no_mangle)]
 pub extern "C" fn FastLoop_unlock(floop_ptr: *mut FastLoop) {
 	let floop = unsafe { &*floop_ptr };
-	unsafe { floop.lock.force_unlock() };
+	unsafe { floop.data.force_unlock() };
 }
