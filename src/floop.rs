d20ed553e817339870b7cfc26d2931358d6e4d72
very-amused <very-amused@pm.me>
2025-10-24 21:02:26 -0400

    Go back to old FastLoop form

--- src/floop.rs
+++ src/floop.rs
@@ -10,26 +10,16 @@ use crate::server::FastServer;
 
 /// An async-loop emulator providing a mutex over FAST operations.
 pub struct FastLoop {
-	data: Mutex<FastLoopData>
-}
-
-struct FastLoopData {
 	runtime: Arc<Runtime>,
+	lock: Mutex<()>, // lock that ensures only 1 callback is run at a time
 	callback_task: Option<JoinHandle<()>>
 }
+
 
 impl FastLoop {
 	pub fn run_callback<F>(&mut self, callback: F)
 	where F: FnOnce() -> () + Send + 'static {
-		let mut inner = self.data.lock();
-		// move guard into the spawned callback
-		// This is why we can't do mutices the proper way.
-		// FIXME
-		inner.callback_task = Some(inner.runtime.spawn_blocking(|| {
-			let guard = self.data.make_guard_unchecked();
-		}));
-
-		mem::forget(inner); // Allow callback to continue with mutex lock
+		todo!()
 	}
 }
 
@@ -38,13 +28,10 @@ impl FastLoop {
 pub extern "C" fn FastLoop_new(srv_ptr: *mut FastServer) -> *mut FastLoop {
 	let srv = unsafe { &*srv_ptr };
 
-	let inner = FastLoopData {
-		runtime:  srv.0.clone(),
-		callback_task: None
-	};
-
 	let floop = Box::new(FastLoop {
-		data: Mutex::new(inner)
+		runtime: srv.0.clone(),
+		lock: Mutex::new(()),
+		callback_task: None
 	});
 
 	Box::leak(floop)
@@ -52,13 +39,13 @@ pub extern "C" fn FastLoop_new(srv_ptr: *mut FastServer) -> *mut FastLoop {
 
 #[unsafe(no_mangle)]
 pub extern "C" fn FastLoop_free(floop_ptr: *mut FastLoop) {
-	let floop = unsafe { Box::from_raw(floop_ptr) };
+	let mut floop = unsafe { Box::from_raw(floop_ptr) };
 
 	// Abort any callback that's running
-	let mut inner = floop.data.lock();
-	if let Some(thr) = &inner.callback_task {
+	let guard = floop.lock.lock();
+	if let Some(thr) = floop.callback_task {
 		thr.abort();
-		inner.callback_task = None;
+		floop.callback_task = None;
 	}
 
 	// floop gets dropped
@@ -66,7 +53,7 @@ pub extern "C" fn FastLoop_free(floop_ptr: *mut FastLoop) {
 
 #[unsafe(no_mangle)]
 pub extern "C" fn FastLoop_lock(floop_ptr: *mut FastLoop) {
-	let guard = unsafe { (&*floop_ptr).data.lock() };
+	let guard = unsafe { (&*floop_ptr).lock.lock() };
 
 	mem::forget(guard);
 }
@@ -74,5 +61,5 @@ pub extern "C" fn FastLoop_lock(floop_ptr: *mut FastLoop) {
 #[unsafe(no_mangle)]
 pub extern "C" fn FastLoop_unlock(floop_ptr: *mut FastLoop) {
 	let floop = unsafe { &*floop_ptr };
-	unsafe { floop.data.force_unlock() };
+	unsafe { floop.lock.force_unlock() };
 }
